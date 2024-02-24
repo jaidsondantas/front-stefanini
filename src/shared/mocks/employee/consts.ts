463068336685ee654ce7338ea50ee647009a2b1a
Jaidson Dantas <jaidsondantas@gmail.com>
2024-02-24 09:24:49 -0300

    feat: add const mock

---
+++ src/shared/mocks/employee/consts.ts
@@ -0,0 +1,6 @@
+import { Employee } from 'src/shared/interfaces/employee.interface';
+
+export const employeesMock: Employee[] = [
+  { id: 1, name: 'John Doe', age: 22, office: 'Developer' },
+  { id: 2, name: 'Jane Smith', age: 22, office: 'Developer' },
+];
